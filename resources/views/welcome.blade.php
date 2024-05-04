cb60948265d219907bc1e46e223f788780d268ba
petrero <petrero@gmail.com>
2024-05-05 00:12:57 +0300

    27.21 All right, what else?  How about the employer logo? That should be rounded, and that's a little hard to see right now, but later that'll be a full image.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
           <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
         </div>
 
-        <img src="http://placehold.it/42/42" alt="">
+        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
       </div>
     </div>
   </section>
