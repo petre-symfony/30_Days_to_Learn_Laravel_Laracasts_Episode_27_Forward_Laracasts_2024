87ba475a4d70092cdf2d0c9b927f920a726cb379
petrero <petrero@gmail.com>
2024-05-05 00:42:53 +0300

    27.26 but maybe we should change this up.  Maybe this will be wrapped within its own div, and then we'll have a span followed by the H3. Yeah, if I take that approach, then I could do something like inline flex item center, and I can even add a little bit of a space in between the square and the heading itself.  Let's have a look. Yeah, and I think that looks pretty good, actually.  The only tweak might be the heading should be bold and a little bit larger than the regular text. And there we go, we have our section headings, cool.

--- resources/views/components/section-heading.blade.php
+++ resources/views/components/section-heading.blade.php
@@ -1,4 +1,7 @@
-<h3>
+<div class="inline-flex items-center gap-x-2">
   <span class="w-2 h-2 bg-white inline-block"></span>
-  {{ $slot }}
-</h3>
+
+  <h3 class="font-bold text-lg">
+    {{ $slot }}
+  </h3>
+</div>
