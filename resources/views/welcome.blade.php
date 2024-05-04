8f631a06d1221860de83b2a1b2111186c7b74719
petrero <petrero@gmail.com>
2024-05-05 01:10:48 +0300

    27.31 Oh, and by the way, I put this in the wrong section. So let's fix that. You were waiting for me to fix that. Like so.  All right, so I think we made pretty good progress here, but we're going a little high on time. So in the next episode, we will work on the expanded job card. And by the way, if you want, why don't you get a headstart and see how your version compares to mine?  Okay, so we're going to wrap up there. Again, I know we're going fast, but that's the way it's done. In the next episode, we will finish up our layout so that we can then begin working on the architecture itself.  So I'm going to take a break, and I will see you in the next episode.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,10 +12,6 @@
 
     <section>
       <x-section-heading>Tags</x-section-heading>
-    </section>
-
-    <section>
-      <x-section-heading>Recent Jobs</x-section-heading>
 
       <div class="mt-6 space-x-1">
         <x-tag>Tag</x-tag>
@@ -29,6 +25,10 @@
         <x-tag>Tag</x-tag>
         <x-tag>Tag</x-tag>
       </div>
+    </section>
+
+    <section>
+      <x-section-heading>Recent Jobs</x-section-heading>
     </section>
   </div>
 </x-layout>
