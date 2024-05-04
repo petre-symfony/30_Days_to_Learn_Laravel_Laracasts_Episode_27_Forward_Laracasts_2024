b7ee4a5af43194184622841140e4151f3b07a5f3
petrero <petrero@gmail.com>
2024-05-05 00:09:34 +0300

    27.20 Okay, next, on the section heading, why don't we give it a margin bottom of six, something like that, just to push it down, and then let's do this for our tags.  So I'll start with just one. I'll add a class of background white. Again, I'm just using alpha here to make it a little quicker for us, about 10%. Let's then add some padding on the left and right, and the top and bottom. Let's make it rounded, and I want the text to be fairly small, so let's use extra small there.  All right, but now what about when we hover over it? Well, we could say, well, when you hover, set the background to 25%, maybe. That looks good, I think.  And by the way, a little tip, I know we don't have much breathing room here, but if you want to transition those colors a bit more cleanly, you could say transition colors, and then set a duration of about 300 milliseconds. So now any colors will have more of a smooth transition, and I don't know if you can see that in the video, but it's definitely smoother.  Okay, so now think about it. I could do three of those, and that looks a little better.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
   <section>
-    <h3>Top Jobs</h3>
+    <h3 class="mb-6">Top Jobs</h3>
 
     <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
       <div class="self-start text-sm">Laracasts</div>
@@ -12,9 +12,9 @@
 
       <div class="flex justify-between items-center mt-auto">
         <div>
-          <a href="#">Tag</a>
-          <a href="#">Tag</a>
-          <a href="#">Tag</a>
+          <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+          <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+          <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
         </div>
 
         <img src="http://placehold.it/42/42" alt="">
