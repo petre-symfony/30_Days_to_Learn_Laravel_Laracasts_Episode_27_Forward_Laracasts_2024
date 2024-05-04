a80a494a45989f525f157de640dad02d74aa555b
petrero <petrero@gmail.com>
2024-05-05 00:18:20 +0300

    27.22 Okay, so this is enough to extract a component. So let's do this.  Let's open our sidebar, and within our views directory, let's add a brand new component, and I'm gonna call this job card. All right, so now I can select all of this and move it over.  Cool, so let's swap it out. x-job-card, and let's create three of them.  Okay, back to the browser, and yeah, now we're repeating that, but notice there's no spacing in between them.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,23 +2,8 @@
   <section>
     <h3 class="mb-6">Top Jobs</h3>
 
-    <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
-      <div class="self-start text-sm">Laracasts</div>
-
-      <div class="py-8 font-bold">
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
-      </div>
-
-      <div class="flex justify-between items-center mt-auto">
-        <div>
-          <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
-          <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
-          <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
-        </div>
-
-        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
-      </div>
-    </div>
+    <x-job-card/>
+    <x-job-card/>
+    <x-job-card/>
   </section>
 </x-layout>
