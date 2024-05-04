6b1bae4668944725192a272e837be8a013835201
petrero <petrero@gmail.com>
2024-05-05 00:23:14 +0300

    27.23 Let's fix that. We'll wrap all of these within a div, and then this time I will use a grid. We'll say for large devices, we want grid calls three. Next, I want a nice little gap in between them, so we'll set, it's basically padding in between each of the job cards.  Yeah, it's starting to take shape, but now notice how everything is right up against the left edge.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,10 @@
   <section>
     <h3 class="mb-6">Top Jobs</h3>
 
-    <x-job-card/>
-    <x-job-card/>
-    <x-job-card/>
+    <div class="grid lg:grid-cols-3 gap-8">
+      <x-job-card/>
+      <x-job-card/>
+      <x-job-card/>
+    </div>
   </section>
 </x-layout>
