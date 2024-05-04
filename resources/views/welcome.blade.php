12baff0b445b36e2e821f8d219c64a2ee6d2fcc2
petrero <petrero@gmail.com>
2024-05-04 23:56:30 +0300

    27.18 For example, the employer should be self-start, so that should be on the far left, and while we're here, let's make the text just a little bit smaller.  Next, for this section, let's give it a little bit of padding. So PY, maybe eight. I want it to be bold, and then down here, yeah, we're gonna do the same thing.  Let's set a class of flex and justify between, and maybe align the items to the center.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,14 @@
     <h3>Top Jobs</h3>
 
     <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
-      <div>Laracasts</div>
-      <div>
+      <div class="self-start text-sm">Laracasts</div>
+
+      <div class="py-8 font-bold">
         <h3>Video Producer</h3>
         <p>Full Time - From $60,000</p>
       </div>
 
-      <div>
+      <div class="flex justify-between items-center">
         <div>
           <a href="#">Tag</a>
           <a href="#">Tag</a>
