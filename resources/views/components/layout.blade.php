2614c86f6ef875505a5f3bdba5edc0d6de5b550f
petrero <petrero@gmail.com>
2024-05-04 23:12:13 +0300

    27.12 So on the nav, I'm gonna set a display of flex, and then I will say justify between.  And that should push this one all the way to the right, this one to the left, and this in the middle. Let's switch back to our project, give it a refresh, and there we go. Cool. Next, why don't we align the items to the center? Items, center. All right, cool.  Next, everything is right up against the edge of the window. So right up here, why don't we set some padding on the left and right of 10? And why don't we switch to a two-up layout? And yeah, that'll save us some time.  All right, next on the nav itself, yeah, if I set a background of red or something, let's give it a little padding on the top and bottom, a bit of breathing room.  And next on the body itself, I want it to be a black color. So I could set class of bg-black, like so. And yeah, now we can at least see the logo a bit better.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,9 +7,9 @@
     <title>Pixel positions</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
-  <body>
-    <div>
-      <nav>
+  <body class="bg-black text-white">
+    <div class="px-10">
+      <nav class="flex justify-between items-center py-4">
         <div>
           <a href="/">
             <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
