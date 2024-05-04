2a70c7be0991be2986d505c952b7c051c21d1913
petrero <petrero@gmail.com>
2024-05-04 23:38:19 +0300

    27.16 So I have a little more work to do here for the layout, but I think we can switch over to our welcome view and get started on the main section for the homepage.  Now, if I switch to Zeppelin, have a look here.  I can see a few blade components we'll need to create. For example, what you see right here, that is a repeating theme where you have a little square, it could be a certain color, and then a heading.  So I see it here, I see it right up here. I see it for all of the headings, actually.  That's something that should probably be a component. Next, the styling for these buttons all seem to be the same. And then we have styling for a job card, and then an alternate styling for like an expanded job card. Okay, so I'm filing these away in my head. All right, so let's do this.  I will begin with one section at a time, top jobs. Okay, so with that in mind, let's wrap it within a section. We'll have some kind of heading that'll say top jobs, and then within a div, it looks like we're gonna have three job cards. Okay, so we'd have something like this ultimately, but for now I'm just gonna work on the first one. So within a card, it looks like we'll have three individual sections, one for the employer name, then a section for the title. I'll hard-code this for now, video producer, and the salary, full-time from 0,000. Finally, at the bottom, we will have a list of tags, something like this, and let's create three of those.  Finally, I will have an image for the employer's logo, but with that in mind, I should probably wrap these tags within a div so that I can push them to the left and the logo to the right, like you see in the image. Now, temporarily for the image, I'm gonna use a placeholder service. I think there's one called placeholder.it, and then I provide the dimensions, and it should be 42 by 42.  All right, and that should be enough to get us going.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,23 @@
 <x-layout>
-    Hello World
+  <section>
+    <h3>Top Jobs</h3>
+
+    <div>
+      <div>Laracasts</div>
+      <div>
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+      </div>
+
+      <div>
+        <div>
+          <a href="#">Tag</a>
+          <a href="#">Tag</a>
+          <a href="#">Tag</a>
+        </div>
+
+        <img src="http://placehold.it/42/42" alt="">
+      </div>
+    </div>
+  </section>
 </x-layout>
