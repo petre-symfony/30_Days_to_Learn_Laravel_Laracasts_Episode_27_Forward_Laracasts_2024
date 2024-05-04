fcde0d42c1cd6da29531c4344b2ae6d3d79a956e
petrero <petrero@gmail.com>
2024-05-04 23:18:16 +0300

    27.13 We'll also set the text to white, but if I switch over to Zeppelin, I believe it's not an exact black. Let's select this, and it looks like this is our black color.  So here's what I'll do. I'm gonna open up tailwind.config.js and extend our colors, and I'll say black should actually be this shade right here. Now, Tailwind should update behind the scenes.  So check this out. If I were instead to set it to red or something like that, you can see now anywhere that I referenced text black or background black, it will now use this value rather than the default. But yeah, let's bring it back.  All right, anyways, back to layouts. And on the nav, there should be a border. So I'll set border B, but that white is a little too strong. So I'll show you a little tip. You can set a border white, but then lower the opacity. And this is useful if you don't have a specific color, but you want it to be a percentage of a color. I could do something like this, and that means white, but set the opacity or the alpha to 10%. And that's what I want in this case. All right, cool.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
   </head>
   <body class="bg-black text-white">
     <div class="px-10">
-      <nav class="flex justify-between items-center py-4">
+      <nav class="flex justify-between items-center py-4 border-b border-white/10">
         <div>
           <a href="/">
             <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
