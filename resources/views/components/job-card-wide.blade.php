c1b208c8804fc386255e2cd4b319d72bba310bbb
petrero <petrero@gmail.com>
2024-05-05 11:03:51 +0300

    28.1 Blade and Tailwind Techniques for Your Laravel Views  All right, welcome back. There's no time to waste, so I'm going to jump right in.  If I switch back to Zeppelin, yeah, we currently have one job card, but right down here we have a second version. And while we could figure out a way to consolidate both of these styles within a single blade component, often it's just easier to create two separate components. And that's what I'm going to do.  So let's go to job card, and if I open up the sidebar here, I will duplicate it, and we'll call this one whatever you want, job card wide, how about that? All right, so let's see.  We should work left to right. So first up on the left is the image itself, the employer's logo. Next we'll have a div that wraps the employer's name, and then the title and the salary requirements.  So I should be able to get rid of this, and why don't we change this to some kind of anchor tag? And actually, we're going to talk more about this at the end of the video, but basically, how can we have multiple anchor tags within a card while still allowing the card itself to be clickable? It's a little bit tricky, but I'll show you how to do it.  All right, so we have the employer name, the job title, the salary, and then finally on the far right, let's just display the tags for now, and we can come back later if we have a little more time, but I'm trying to be as quick as we can here.  All right, that's the general shape. All right, so of course there's more to do, but let's see this in the browser. So here's our main welcome page where we have the tags and then the recent jobs. I'm going to grab this div here, and we'll have a new one, this time for job card wide. All right, and why don't we do three, but this time we'll have space between them vertically, so maybe, I don't know, space-y-3.  All right, let's have a look. So we come back to the browser, and whoa, we have a gigantic image, and this is probably related to Flexbox.

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+  <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+
+  <div>
+    <a class="self-start text-sm">Laracasts</a>
+
+    <h3>Video Producer</h3>
+    <p>Full Time - From $60,000</p>
+  </div>
+
+
+  <div>
+    <x-tag>Tag</x-tag>
+    <x-tag>Tag</x-tag>
+    <x-tag>Tag</x-tag>
+  </div>
+
+</div>
