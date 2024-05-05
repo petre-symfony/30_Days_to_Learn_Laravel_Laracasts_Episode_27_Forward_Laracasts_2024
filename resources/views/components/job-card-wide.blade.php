a0a3f50fd530f4356b968891c7c16189fb5a1dc3
petrero <petrero@gmail.com>
2024-05-05 12:54:33 +0300

    28.19 Okay, but next, I would also have to apply those hover, those border styles to the wide card as well. And that's not ideal.  But yeah, let's just see what that would look like. Notice I'm doing a bit of duplication here, which is a sign. So let's go to job card wide. And I'm going to paste those styles in here.  And then for the video producer, group hover, text blue, 800. You know what, I think I used the wrong ones here. Notice I have 600 and 800 up here. We want those to be consistent. Okay, so now if I hover over it, that looks good.  Maybe it's too dark. Maybe it should be 600. But we're not going to get stuck in the weeds here. You get the idea.

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+<div class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-800 group">
   <div>
     <x-employer-logo />
   </div>
@@ -6,7 +6,7 @@
   <div class="flex-1 flex flex-col">
     <a class="self-start text-sm text-gray-400">Laracasts</a>
 
-    <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+    <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">Video Producer</h3>
     <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
   </div>
 
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -2,7 +2,7 @@
   <div class="self-start text-sm">Laracasts</div>
 
   <div class="py-8">
-    <h3 class="group-hover:text-blue-600 text-xl font-bold">Video Producer</h3>
+    <h3 class="group-hover:text-blue-800 text-xl font-bold">Video Producer</h3>
     <p class="text-sm mt-4">Full Time - From $60,000</p>
   </div>
 
