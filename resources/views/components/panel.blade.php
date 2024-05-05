f1ee0c2395a6d81162d90a85a413ddb4946b0b74
petrero <petrero@gmail.com>
2024-05-05 13:19:51 +0300

    28.23 However, if we just call attributes as a function, it's going to merge it. So this is sort of a shorthand for attributes merge. And yeah, that would work.

--- resources/views/components/panel.blade.php
+++ resources/views/components/panel.blade.php
@@ -1,5 +1,5 @@
 <div
-    {{ $attributes->merge([
+    {{ $attributes([
 	    'class' => 'p-4 bg-white/5 rounded-xl border border-transparent
         hover:border-blue-800 group transition-colors duration-300'
     ]) }}
