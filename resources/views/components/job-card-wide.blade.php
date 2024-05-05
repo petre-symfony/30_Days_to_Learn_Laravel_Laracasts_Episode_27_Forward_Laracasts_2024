5cc0602895921090ce2ae74bcb363e12e1bf4924
petrero <petrero@gmail.com>
2024-05-05 11:56:13 +0300

    28.9 All right, so let's continue with our wide job card. Job card wide, and yeah, of course, the title itself should be bold. It should be large.  And then for the paragraph, yeah, this is going to be very similar. I also want it to be small and gray.  Okay, so if we switch back, yeah, that's looking fairly good, but notice how everything is grouped at the top. Ideally, I want to push the salary to the bottom of this section down here, like you see in the image.

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -6,8 +6,8 @@
   <div class="flex-1">
     <a class="self-start text-sm text-gray-400">Laracasts</a>
 
-    <h3>Video Producer</h3>
-    <p>Full Time - From $60,000</p>
+    <h3 class="font-bold text-lg">Video Producer</h3>
+    <p class="text-sm text-gray-400">Full Time - From $60,000</p>
   </div>
 
 
