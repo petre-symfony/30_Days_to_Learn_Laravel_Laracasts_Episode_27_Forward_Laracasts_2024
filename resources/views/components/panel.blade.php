99727bc29bb727ae5efa01fd904d731263f7dd45
petrero <petrero@gmail.com>
2024-05-05 13:18:06 +0300

    28.22 And then, of course, we've learned if we want to merge these, well, we would have to do something like this.  And I'll show you a little shortcut. So up until this point, we've been saying attributes merge, and then we set the default class like so, and that would do the trick.

--- resources/views/components/panel.blade.php
+++ resources/views/components/panel.blade.php
@@ -1,6 +1,8 @@
 <div
-    class="p-4 bg-white/5 rounded-xl border border-transparent
-      hover:border-blue-800 group transition-colors duration-300"
+    {{ $attributes->merge([
+	    'class' => 'p-4 bg-white/5 rounded-xl border border-transparent
+        hover:border-blue-800 group transition-colors duration-300'
+    ]) }}
 >
   {{ $slot }}
 </div>
