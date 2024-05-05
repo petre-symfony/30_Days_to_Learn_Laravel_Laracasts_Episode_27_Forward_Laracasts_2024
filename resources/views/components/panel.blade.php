470ed4049284e670263c7fb9453356b68934cce7
petrero <petrero@gmail.com>
2024-05-05 13:29:42 +0300

    28.25 Or keep it even simpler classes. Now you can reference that variable within your template here.  So you could do something like this just to make it a little easier to consume, right, especially on a low resolution like I have those utility classes go off the screen very quickly.  So this is an option if you want. All right, so back to Arc. And yeah, notice now we have the exact same thing as we had before, but we have consolidated those stylings within a panel component. Pretty cool.  So now, for example, if we go back to welcome, if we were to add a new section for something totally different, I could open up a panel and say, hello there, whatever you want, and you'll see it will receive those same styles. Yeah, it seems like that's a repeating pattern for this site. So it makes sense to extract it into its own component. Let's get rid of it.

--- resources/views/components/panel.blade.php
+++ resources/views/components/panel.blade.php
@@ -1,10 +1,10 @@
 @php
- $defaultClasses = 'p-4 bg-white/5 rounded-xl border border-transparent
+ $classes = 'p-4 bg-white/5 rounded-xl border border-transparent
         hover:border-blue-800 group transition-colors duration-300';
 @endphp
 <div
     {{ $attributes([
-	    'class' => ''
+	    'class' => $classes
     ]) }}
 >
   {{ $slot }}
