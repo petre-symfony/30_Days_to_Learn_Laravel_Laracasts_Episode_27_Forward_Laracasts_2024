b3cdc0b70b1c3040f80c112c688122082e96aa39
petrero <petrero@gmail.com>
2024-05-05 13:01:49 +0300

    28.20 Now, once again, if you want to transition these, so it's a nice, smooth transition from transparent to blue, do this once again, set transition colors duration. And let's make it really long so you can clearly see it. So come back and well, the title isn't quite right, but notice the border is definitely transitioning ends. Yeah, I guess I forgot the title has to receive the same thing. Transition colors, duration, and this time let's make it much quicker. A third of a second often seems to be a good value to go with, a third of a second or 300 milliseconds. Yeah, if you like that, it's just a little bit smoother.  Okay, so once again, we'd have to do the same thing for the job card wide. This feels kind of gross, doesn't it? Then right down here, the exact same thing. And now that works as well. It's not the end of the world, but it seems like we could have more than one card while we already do.

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,4 +1,7 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-800 group">
+<div
+    class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent
+      hover:border-blue-800 group transition-colors duration-300"
+>
   <div>
     <x-employer-logo />
   </div>
@@ -6,7 +9,7 @@
   <div class="flex-1 flex flex-col">
     <a class="self-start text-sm text-gray-400">Laracasts</a>
 
-    <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">Video Producer</h3>
+    <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
     <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
   </div>
 
