ea1b9633997066333f9612391245f01a93556562
petrero <petrero@gmail.com>
2024-05-05 13:14:08 +0300

    28.21 But in a real pixel positions application, we could have many cards that have this styling. They're almost like panels, or you can call them cards if you want. I often call them panels.  So with that in mind, could this be a candidate for a component? Let's give it a shot. Create a component called panel, and I'm going to use a split view here. Let's take the wrapper here, and then we can have a slot like so. All right, let's see how that looks.  If we took that route, I could swap it out with our x-panel component. And now we've effectively consolidated all of those styles. So let's see how job card wide looks.  Yep. No difference. Good. Next, I can go to job card, and we're going to do the exact same thing.  However, I want to make sure, is there any unique classes here? So did we duplicate the classes? Here, I'm just going to paste this in. Yeah, we actually have a couple differences. Yeah, that would be the flex information and the gap. Okay, so that should be excluded. All right, let's do this. I'm going to change this to x-panel. There we go. And then I'll get rid of everything except for the unique parts. So it sounds like the unique parts are that within the panel, we want to display a flex. So get rid of. Yeah. And then if I click through to panel, this is unique to job card wide, so I don't want to assume it, but everything else should be the same. All right, so job card wide, pass these in.

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,7 +1,4 @@
-<div
-    class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent
-      hover:border-blue-800 group transition-colors duration-300"
->
+<x-panel class="flex gap-x-6">
   <div>
     <x-employer-logo />
   </div>
@@ -20,4 +17,4 @@
     <x-tag>Tag</x-tag>
   </div>
 
-</div>
+</x-panel>
