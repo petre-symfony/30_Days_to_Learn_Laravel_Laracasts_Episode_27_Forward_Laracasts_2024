8d25dd16348e27fd527ff45e7676d6157349b052
petrero <petrero@gmail.com>
2024-05-05 13:24:04 +0300

    28.24 Also keep in mind, if you want, you can do things like this, where you could open up @php blocks like this, and then you could say default classes

--- resources/views/components/panel.blade.php
+++ resources/views/components/panel.blade.php
@@ -1,7 +1,10 @@
+@php
+ $defaultClasses = 'p-4 bg-white/5 rounded-xl border border-transparent
+        hover:border-blue-800 group transition-colors duration-300';
+@endphp
 <div
     {{ $attributes([
-	    'class' => 'p-4 bg-white/5 rounded-xl border border-transparent
-        hover:border-blue-800 group transition-colors duration-300'
+	    'class' => ''
     ]) }}
 >
   {{ $slot }}
