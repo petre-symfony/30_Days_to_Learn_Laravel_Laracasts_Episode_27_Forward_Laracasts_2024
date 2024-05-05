c08011e47584fef2cef90d188cb44c422aedb2fb
petrero <petrero@gmail.com>
2024-05-05 12:48:10 +0300

    28.18 But the salary is still too bold. Let's see. Oh, that's because we applied it to the div.  Okay, let's apply it only to the title. And then the salary itself can be a base level.Yeah, and that's looking a little better.

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,8 +1,8 @@
 <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group">
   <div class="self-start text-sm">Laracasts</div>
 
-  <div class="py-8 font-bold">
-    <h3 class="group-hover:text-blue-600 text-xl">Video Producer</h3>
+  <div class="py-8">
+    <h3 class="group-hover:text-blue-600 text-xl font-bold">Video Producer</h3>
     <p class="text-sm mt-4">Full Time - From $60,000</p>
   </div>
 
