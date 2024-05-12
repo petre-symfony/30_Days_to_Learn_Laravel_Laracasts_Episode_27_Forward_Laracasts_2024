f6ca9cf96bfdb145559d502cc5e806fa54612b48
petrero <petrero@gmail.com>
2024-05-12 20:20:51 +0300

    29.26 Okay? So just a couple final things. I'm gonna go into job card and make all of these dynamic. So we'll say job, employer name. We want job title, job salary, and then tag. We no longer have to pass this in. And then employer logo, we're gonna come back to that.  Okay, now let's go to the wide version of the card, and I'm gonna do the exact same thing, job employer. So notice we do have an N plus one issue, uh, right now because we haven't eager loaded the employer relationship. So for each item in this loop, we have to perform a new query to fetch the employer. So keep that in mind. Job title, job salary, and same thing here.  All right, back to arc. And now these are unique the way we'd expect.

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -5,10 +5,10 @@
   </div>
 
   <div class="flex-1 flex flex-col">
-    <a class="self-start text-sm text-gray-400">Laracasts</a>
+    <a class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
 
-    <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
-    <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+    <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">{{ $job->title }}</h3>
+    <p class="text-sm text-gray-400 mt-auto">{{ $job->schedule }} - From {{ $job->salary }}</p>
   </div>
 
 
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,10 +1,10 @@
 @props(['job'])
 <x-panel class="flex flex-col text-center">
-  <div class="self-start text-sm">Laracasts</div>
+  <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
   <div class="py-8">
-    <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>
-    <p class="text-sm mt-4">Full Time - From $60,000</p>
+    <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>
+    <p class="text-sm mt-4">{{ $job->schedule }} - From {{ $job->salary }}</p>
   </div>
 
   <div class="flex justify-between items-center mt-auto">
