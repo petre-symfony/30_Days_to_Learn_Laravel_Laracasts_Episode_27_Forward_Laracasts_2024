4da02dcccfedebc765751413f86d4e2a166ae7ca
petrero <petrero@gmail.com>
2024-05-12 19:30:22 +0300

    29.22 All right, back to job controller. Now as you can imagine, and let's update this, we would need to pass jobs to our job listing page. And I'm also imagining that we need to pass a list of tags. So let's do that one as well. All right, let's keep it very simple.  We can now reference our job model and say, gimme all of them though of course, as we've learned, uh, in a real project, we would want to paginate them. But for now, a select star is fine. We'll do the exact same thing for our tags.  Okay, let's go into our view and you'll remember that, yeah, in the last episode everything was static. We're just hard coding all of these, but now we can swap them out. Let's begin with how about tags? We can say for each tags as tag then render this blade component. But now I want to pass in, uh, the associated tag, like this tag equals tag or a little tip in situations like this where your parameter name and the variable name are the same.

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreJobRequest;
 use App\Http\Requests\UpdateJobRequest;
 use App\Models\Job;
+use App\Models\Tag;
 
 class JobController extends Controller {
 	/**
 	 * Display a listing of the resource.
 	 */
 	public function index() {
-		return view('jobs.index');
+		return view('jobs.index', [
+			'jobs' => Job::all(),
+			'tags' => Tag::all()
+		]);
 	}
 
 	/**
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -22,16 +22,9 @@
       <x-section-heading>Tags</x-section-heading>
 
       <div class="mt-6 space-x-1">
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
+        @foreach($tags as $tag)
+          <x-tag :tag="$tag" />
+        @endforeach
       </div>
     </section>
 
