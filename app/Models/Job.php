b33292b5b7ff07d515289545731fc81ec930d71f
petrero <petrero@gmail.com>
2024-05-12 19:05:22 +0300

    29.19 Let's go back into job and let's see that relationship is working, but right now the tag method doesn't do anything at all. So let's update this. If I accept a tag name here, we'll call it name. Now there's a couple ways to do this. Uh, I'm gonna reference the tag model. So I'm gonna say give me the first tag, uh, from the database table that has this name or if you couldn't find one with that name, create or persist one in that table.  So I'm gonna say the name is name, alright? Does that make sense? Give me the tag from that table. And if you couldn't find the tag with that name, create one and then give me the tag. Alright, next I'm gonna interact with my pivot table. I can say this tags and attach a new tag here. Just like that. Alright, finally these squiggly lines. My editor just wants return types. Again, these are optional depending on how you like to build your applications, but we could import these and simplify.  Alright, let's run our tests again and it fails. But I want you to notice right here, yeah, check this out. Add name to the fillable property to allow mass assignment on tag. Yes. So notice when we take this approach, it almost feels like the tests are telling you what the next thing you need to do is. And that's one of the huge benefits to this approach.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model {
 	use HasFactory;
@@ -13,11 +14,13 @@ class Job extends Model {
 		return $this->belongsTo(Employer::class);
 	}
 
-	public function tag() {
+	public function tag(string $name) {
+		$tag = Tag::firstOrCreate(['name' => $name]);
 
+		$this->tags()->attach($tag);
 	}
 
-	public function tags() {
+	public function tags(): BelongsToMany {
 		return $this->belongsToMany(Tag::class);
 	}
 }
