bd2541dad8d24616d6895d52ae48644797bfd482
petrero <petrero@gmail.com>
2024-05-12 15:56:14 +0300

    29.6 So now I'm gonna set up those relationships. Let's start with employer. An employer belongs to a user. So return, this is a belongs to relationship. And then we reference, uh, the associated model. And if you want you can add a return type or you can omit it. Just depends on what system you wanna follow.  Alright, now let's do the inverse. So in this case, the user is actually like the representative for the employer. So it's not a belongs to relationship, it's actually a has one relationship. So a user has one employer, like, so.  All right, next let's switch over to job. So what are the relationships for a job? Well, as we discussed, a job belongs to an employer, right? Of course it does. Alright, next, yeah, once again, let's do the inverse. If I have an employer in stents and I wanna see all jobs associated with that one employer and no one else, well let's do that. Jobs. And this is going to be a has many relationship. An employer has many jobs. And again, if you want you can add a type here. Just make sure that you import it, uh, at the top here.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,7 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model {
 	use HasFactory;
+
+	public function employer(): BelongsTo {
+		return $this->belongsTo(Employer::class);
+	}
 }
