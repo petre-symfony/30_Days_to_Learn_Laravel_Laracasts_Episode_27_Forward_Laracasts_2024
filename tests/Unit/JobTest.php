29d40619d863ae576108970a76b5073461ddb35f
petrero <petrero@gmail.com>
2024-05-12 18:04:56 +0300

    29.12 All right, so I'll show you an example.  It belongs to an employer. How do I arrange the world here? Well, if it belongs to an employer, then I probably need an employer to perform this action, right? Let's use a factory for that employer factory create. Alright, next we are using it, it refers to a job, but I don't have a job currently. So let's do that as well. Let's say job equals job factory create. Now when I run this, it is true, it'll set up its own relationship to an employer, but for this particular test, I wanna be a little more explicit. So if I ever want to override anything declared within the factory, then I pass that as an array to the create method. So in our case, I want to override employer like so, okay, and while we're here, let's clean things up.  All right. So now step one, arrange is complete. Next up perform an action. So this is where I interact with the code base in the way that I want in our case job employer. Okay? So often your action and your assertion will be separate, but in this particular case we can group them into one arrange or I'm sorry, act and assert.  All right? So I could say expect job employer to be this like you see here. So here's what we could do. Job employer is employer. We learned about that method many episodes ago. I expect that to be true. So yeah, if the IS method is new to you, it's checking if what you pass here is the current instance. So that will either return true or false. In our case, we expect it to be true.  All right, let's run our test php artisan test. But it fails for a confusing reason.   A facade root has not been set.

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,9 +1,9 @@
 <?php
 
 it('belongs to an employer', function () {
-	//AAA
-
 	//Arange
-	//Act
-	//Assert
+	$employer = \App\Models\Employer::factory()->create();
+	$job = \App\Models\Job::factory()->create(['employer_id' => $employer->id]);
+	//Act and Assert
+	expect($job->employer()->is($employer))->toBeTrue();
 });
