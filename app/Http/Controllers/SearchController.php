7dca83de35e4a9c6b4016750b30de182bae5784c
petrero <petrero@gmail.com>
2024-05-17 21:48:00 +0300

    30.16 And this will be fairly primitive. We're not gonna use full text search or anything special.  Uh, we'll just say look for any job where the title is, like, uh, this search, uh, peram and that should be enough. So let's say job where the title is, like the request query, but then I'm gonna say, yeah, any number of characters can come before that query and any number of characters can come after it. So as long as this search query exists somewhere within the title, I want it included in the results set.  So we will fetch that and then why don't we return that as Json so we can have a look in the browser. Let's start from scratch. Let's look for something related to how about teacher? All right, we run it, let's do pretty print and we get one result.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
+
 class SearchController extends Controller {
 	public function __invoke() {
-		dd(request('q'));
+		$json = Job::where('title', 'like', '%'.request('q').'%')->get();
+
+		return $json;
 	}
 }
