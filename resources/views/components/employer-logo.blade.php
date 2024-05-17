9ad84e1b24f745e9ff5d8cd192f6201dd9472997
petrero <petrero@gmail.com>
2024-05-17 22:37:14 +0300

    30.27 All right, let's go into employer logo. And yet we accept a width, but we never used it. So let's do that now, come back and that looks good to me.  Alright, next, let's go into job controller.

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,2 +1,2 @@
 @props(['employer', 'width' => 90])
-<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl">
+<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}">
